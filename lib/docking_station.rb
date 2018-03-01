5ca1ad099d5e6602925cc56a1e35177fe7d5d818
Royston Henson <rlqhenson@hotmail.com>
2018-03-01 15:02:09 +0000

    Add feature: docking station docks bikes

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,17 @@
 require_relative 'bike'
 
 class DockingStation
+    attr_reader :bike_stand
+
+  def initialize
+    @bike_stand = []
+  end
+
   def release_bike
     Bike.new
+  end
+
+  def dock_bike(bike)
+    @bike_stand << bike
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,11 +1,20 @@
 require 'docking_station.rb'
 
 describe DockingStation do
+  let(:bike) { Bike.new }
+
   describe '#release_bike' do
       it { is_expected.to respond_to (:release_bike) }
 
       it 'releases a bike' do
         expect(subject.release_bike).to be_a(Bike)
+      end
+  end
+
+  describe  '#dock_bike' do
+      it 'docks a bike' do
+        subject.dock_bike(bike)
+        expect(subject.bike_stand).to eq([bike])
       end
   end
 end
