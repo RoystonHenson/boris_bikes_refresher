ee94f3d6c9faca13078e6f9cef7587a6a188013e
Royston Henson <rlqhenson@hotmail.com>
2018-03-01 14:23:34 +0000

    Add feature: bikes are working

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,7 +1,14 @@
 require 'bike'
 
 describe Bike do
+
   describe '#working?' do
     it { expect(subject).to respond_to (:working?) }
+
+    context 'given that bikes are working' do
+      it 'returns true' do
+        expect(subject.working?).to eq(true)
+      end
+    end
   end
 end
