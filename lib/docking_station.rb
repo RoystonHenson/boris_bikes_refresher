d621bb3b7cd337c554311658ebe128e80828d7e6
Royston Henson <rlqhenson@hotmail.com>
2018-03-02 00:56:33 +0000

    refactor docking station, add user stories to read me

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,10 +1,10 @@
 require_relative 'bike'
 
 class DockingStation
-  attr_reader :bikes
-  attr_accessor :capacity
 
   DEFAULT_CAPACITY = 20
+
+  attr_reader :bikes, :capacity
 
   def initialize(capacity=DEFAULT_CAPACITY)
     @bikes = []
@@ -32,6 +32,6 @@ class DockingStation
   end
 
   def full?
-     @bikes.count >= DEFAULT_CAPACITY
+     @bikes.count >= capacity
   end
 end
