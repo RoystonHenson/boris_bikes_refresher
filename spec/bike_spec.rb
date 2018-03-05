0733890d97f25640acec05f8259e700cc3cd8d94
Royston Henson <rlqhenson@hotmail.com>
2018-03-05 16:07:04 +0000

    Add user stories to read me

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -8,8 +8,6 @@ describe Bike do
   end
 
   describe '#working?' do
-    it { expect(subject).to respond_to (:working?) }
-
     context 'given the bike is working' do
       it 'returns true' do
         expect(subject.working?).to eq(true)
