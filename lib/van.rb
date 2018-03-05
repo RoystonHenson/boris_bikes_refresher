8b37030cf1e68e6689bf86a0136fada5d43171ef
Royston Henson <rlqhenson@hotmail.com>
2018-03-05 17:26:00 +0000

    Add feature: van removes broken bikes from docking station

--- lib/van.rb
+++ lib/van.rb
@@ -1,2 +1,9 @@
 class Van
+
+  def collect_broken_bikes(docking_station)
+    @broken_bikes = docking_station.bikes.select { |x|
+      x.working == false
+    }
+    docking_station.bikes.reject! { |x| x.working == false}
+  end
 end
--- spec/van_spec.rb
+++ spec/van_spec.rb
@@ -2,10 +2,15 @@ require 'van'
 
 describe Van do
   let(:bike)   { double('bike', working: true) }
-  let(:bike_2) { double('bike', working: false) }
+  let(:bike_2) { double('bike 2', working: false) }
   let(:docking_station) { double('docking station', bikes: [bike, bike_2]) }
 
   describe '#collect_broken_bikes' do
+    it { is_expected.to respond_to(:collect_broken_bikes) }
 
+    it 'removes broken bikes from docking station' do
+      subject.collect_broken_bikes(docking_station)  
+      expect(docking_station.bikes).to eq([bike])
+    end
   end
 end
